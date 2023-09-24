b07b4c2c0b360759fc93ebf9816e8aa73d5ff324
Agus Anggarwanto <agusanggarwanto@gmail.com>
2023-09-24 11:54:57 +0700

    random

---
+++ util/random.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	source := rand.NewSource(int64(time.Now().UnixNano()))
+	rand.New(source)
+}
+
+func RandomName() string {
+	var sb strings.Builder
+	abjad := "abcdefghijklmnopqrstuvwxyz"
+
+	for i := 0; i < 5; i++ {
+		sb.WriteByte(abjad[rand.Intn(len(abjad))])
+	}
+	return sb.String()
+}
+
+func RandomSaldo() int64 {
+	var min int64 = 10000
+	var max int64 = 10000000
+
+	return min + (rand.Int63n(max - min + 1))
+}
